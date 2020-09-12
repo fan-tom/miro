2900ad7d1936321f3bc39d7ca9337f2fea42b6fd
Alexey Gerasimov <lokomot476@gmail.com>
2020-09-12 23:02:02 +0500

    Implement search in area for in-memory repo impl, based on tree maps as indices over all widget boundaries (left, top, right and bottom). It provides O(log n) search in rectangular area

--- inmemory_repository/src/main/java/org/fantom/repository/InMemoryWidgetRepository.java
+++ inmemory_repository/src/main/java/org/fantom/repository/InMemoryWidgetRepository.java
@@ -5,6 +5,7 @@ import org.fantom.repositories.widget.AtomicAction;
 import org.fantom.repositories.widget.AtomicFunction;
 import org.fantom.repositories.widget.IdGenerator;
 import org.fantom.repositories.widget.WidgetRepository;
+import org.fantom.repositories.widget.dto.Area;
 import org.fantom.repositories.widget.exceptions.ZIndexConflictException;
 import org.fantom.repositories.widget.dto.WidgetCreateDto;
 
@@ -12,6 +13,16 @@ import java.util.*;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 import java.util.stream.StreamSupport;
+
+class Pair<T1, T2> {
+    public final T1 first;
+    public final T2 second;
+
+    Pair(T1 first, T2 second) {
+        this.first = first;
+        this.second = second;
+    }
+}
 
 public class InMemoryWidgetRepository<ID> implements WidgetRepository<ID> {
     // Thread-unsafe impl of widget repository to use in runAtomically without synchronization overhead
@@ -21,25 +32,63 @@ public class InMemoryWidgetRepository<ID> implements WidgetRepository<ID> {
         private final TreeMap<Integer, WidgetDao<ID>> widgetsByZIndex;
         private final IdGenerator<ID> idGenerator;
 
-        public InternalInMemoryWidgetRepository(IdGenerator<ID> idGenerator) {
+        // Indexes over widgets coordinates, to easy filter by area borders
+        private final TreeMap<Integer, HashSet<WidgetDao<ID>>> leftIndex;
+        private final TreeMap<Integer, HashSet<WidgetDao<ID>>> rightIndex;
+        private final TreeMap<Integer, HashSet<WidgetDao<ID>>> topIndex;
+        private final TreeMap<Integer, HashSet<WidgetDao<ID>>> bottomIndex;
+
+        protected InternalInMemoryWidgetRepository(Map<ID, WidgetDao<ID>> widgets, IdGenerator<ID> idGenerator) {
+            this.widgetsById = widgets;
             this.idGenerator = idGenerator;
-            this.widgetsById = new HashMap<>();
             this.widgetsByZIndex = new TreeMap<>();
+
+            this.leftIndex = new TreeMap<>();
+            // we always require widget right edge to have x-coordinate to be LESS than the search area right edge's one
+            this.rightIndex = new TreeMap<>(Collections.reverseOrder());
+            // the same for top: widget top edge y-coordinate must be LESS than the search area top edge's one
+            // this reverse order allows to always take tailMap when searching for widgets
+            this.topIndex = new TreeMap<>(Collections.reverseOrder());
+            this.bottomIndex = new TreeMap<>();
+        }
+
+        public InternalInMemoryWidgetRepository(IdGenerator<ID> idGenerator) {
+            this(new HashMap<>(), idGenerator);
         }
 
         public InternalInMemoryWidgetRepository(Iterable<Widget<ID>> widgets, IdGenerator<ID> idGenerator) throws ZIndexConflictException {
-            this.widgetsById = StreamSupport
-                    .stream(widgets.spliterator(), true)
-                    .map(WidgetDao::new)
-                    .collect(Collectors.toMap(w -> w.id, w -> w));
-            this.idGenerator = idGenerator;
-            this.widgetsByZIndex = new TreeMap<>();
-            for (var widget: widgetsById.values()) {
+            this(StreamSupport
+                            .stream(widgets.spliterator(), true)
+                            .map(WidgetDao::new)
+                            .collect(Collectors.toMap(w -> w.id, w -> w)),
+                    idGenerator);
+            for (var widget : widgetsById.values()) {
                 var existing = widgetsByZIndex.putIfAbsent(widget.zIndex, widget);
                 if (existing != null) {
                     throw new ZIndexConflictException(widget.zIndex);
                 }
+
+                addToIndexes(widget);
             }
+        }
+
+        protected void addToIndexes(WidgetDao<ID> widget) {
+            leftIndex.computeIfAbsent(widget.x, k -> new HashSet<>()).add(widget);
+            rightIndex.computeIfAbsent(widget.x + widget.width, k -> new HashSet<>()).add(widget);
+            topIndex.computeIfAbsent(widget.y + widget.height, k -> new HashSet<>()).add(widget);
+            bottomIndex.computeIfAbsent(widget.y, k -> new HashSet<>()).add(widget);
+        }
+
+        protected void removeFromIndexes(WidgetDao<ID> widget) {
+            Stream.of(
+                    new Pair<>(rightIndex, widget.x),
+                    new Pair<>(leftIndex, widget.x + widget.width),
+                    new Pair<>(topIndex, widget.y + widget.height),
+                    new Pair<>(bottomIndex, widget.y)
+            )
+                    .map(pair -> pair.first.get(pair.second))
+                    .filter(Objects::nonNull)
+                    .forEach(set -> set.remove(widget));
         }
 
         @Override
@@ -51,6 +100,8 @@ public class InMemoryWidgetRepository<ID> implements WidgetRepository<ID> {
                 throw new ZIndexConflictException(widgetDao.zIndex);
             }
             widgetsById.put(widgetDao.id, widgetDao);
+            addToIndexes(widgetDao);
+
             return widget;
         }
 
@@ -80,25 +131,20 @@ public class InMemoryWidgetRepository<ID> implements WidgetRepository<ID> {
             if (oldWidget == null) {
                 return Optional.empty();
             } else {
-                if (oldWidget.zIndex != widget.zIndex) {
-                    var widgetByZIndex = widgetsByZIndex.get(widget.zIndex);
-                    if (widgetByZIndex == null) {
-                        // zIndex is free, remove old mapping and update widget
-                        widgetsByZIndex.remove(oldWidget.zIndex);
-                        // other fields are updated later
-                        oldWidget.zIndex = widget.zIndex;
-                        widgetsByZIndex.put(oldWidget.zIndex, oldWidget);
+                var newWidget = new WidgetDao<>(widget);
+                if (oldWidget.zIndex != newWidget.zIndex) {
+                    if (!widgetsByZIndex.containsKey(newWidget.zIndex)) {
+                        // zIndex is free
+                        widgetsByZIndex.put(newWidget.zIndex, oldWidget);
                     } else {
                         // zIndex already occupied by other widget
-                        throw new ZIndexConflictException(widget.zIndex);
+                        throw new ZIndexConflictException(newWidget.zIndex);
                     }
                 }
 
-                oldWidget.x = widget.x;
-                oldWidget.y = widget.y;
-                oldWidget.width = widget.width;
-                oldWidget.height = widget.height;
-                oldWidget.updatedAt = widget.updatedAt;
+                widgetsById.put(newWidget.id, newWidget);
+                removeFromIndexes(oldWidget);
+                addToIndexes(newWidget);
 
                 return Optional.of(widget);
             }
@@ -107,7 +153,7 @@ public class InMemoryWidgetRepository<ID> implements WidgetRepository<ID> {
         @Override
         public void save(Iterable<Widget<ID>> widgets) throws ZIndexConflictException {
             // TODO: what if exception thrown in the middle?
-            for (var widget: widgets) {
+            for (var widget : widgets) {
                 save(widget);
             }
         }
@@ -117,13 +163,36 @@ public class InMemoryWidgetRepository<ID> implements WidgetRepository<ID> {
             return Optional.ofNullable(widgetsById.get(id)).map(WidgetDao::toWidget);
         }
 
+        @Override
+        public Stream<Widget<ID>> getAll() {
+            return widgetsByZIndex.values().stream().map(WidgetDao::toWidget);
+        }
+
+        @Override
+        public Stream<Widget<ID>> getInArea(Area area) {
+            var result = leftIndex.tailMap(area.left).values().stream().flatMap(Collection::stream).collect(Collectors.toSet());
+            return Stream.of(new Pair<>(rightIndex, area.right), new Pair<>(topIndex, area.top), new Pair<>(bottomIndex, area.bottom))
+                    // avoid synchronization of resulting hashset
+                    .sequential()
+                    .reduce(result, (res, pair) -> {
+                                res.retainAll(pair.first.tailMap(pair.second).values().stream().flatMap(Collection::stream).collect(Collectors.toList()));
+                                return res;
+                            },
+                            (set1, set2) -> {
+                                // get exception on parallel execution
+                                throw new RuntimeException("Sequential reduce executed in parallel");
+                            })
+                    .stream()
+                    .map(WidgetDao::toWidget);
+        }
+
         protected WidgetDao<ID> deleteAndReturnByIdInternal(ID id) {
             var widget = widgetsById.remove(id);
             if (widget != null) {
                 widgetsByZIndex.remove(widget.zIndex);
-                return widget;
+                removeFromIndexes(widget);
             }
-            return null;
+            return widget;
         }
 
         @Override
@@ -141,11 +210,11 @@ public class InMemoryWidgetRepository<ID> implements WidgetRepository<ID> {
         public void deleteAll() {
             widgetsByZIndex.clear();
             widgetsById.clear();
-        }
 
-        @Override
-        public Stream<Widget<ID>> getAll() {
-            return widgetsByZIndex.values().stream().map(WidgetDao::toWidget);
+            leftIndex.clear();
+            rightIndex.clear();
+            topIndex.clear();
+            bottomIndex.clear();
         }
 
         @Override
@@ -164,7 +233,7 @@ public class InMemoryWidgetRepository<ID> implements WidgetRepository<ID> {
                 // no widgets above given zIndex or zIndex is free
                 return;
             }
-            if (!(widgetsByZIndex.containsKey(zIndex+1))){
+            if (!(widgetsByZIndex.containsKey(zIndex + 1))) {
                 // fast version, zIndex is used, but next value is not
                 var widget = equalOrUpperKey.getValue();
                 var newZIndex = zIndex + 1;
@@ -178,7 +247,7 @@ public class InMemoryWidgetRepository<ID> implements WidgetRepository<ID> {
                 for (var z: overlyingWidgets.keySet()) {
                     if (z - topUsedZIndex > 1) {
                         break;
-                    } else{
+                    } else {
                         topUsedZIndex = z;
                     }
                 }
@@ -206,10 +275,10 @@ public class InMemoryWidgetRepository<ID> implements WidgetRepository<ID> {
             action.run(this);
         }
     }
+
     private final InternalInMemoryWidgetRepository<ID> internal;
-    // acquire read lock if you don't need to keep maps unchanged for long time, like when you add/remove elements and
-    // don't care if concurrent adds/removes are performed, or you simply read
-    // otherwise, acquire write lock (like when you need to be sure map is not changed between different reads/writes)
+    // rwlock to protect internal repo from concurrent writes or read/write
+    // couldn't use thread-safe map impls as multiple maps must be changed concurrently
     private final ClosableReentrantReadWriteLock rwLock = new ClosableReentrantReadWriteLock(true);
 
     public InMemoryWidgetRepository(Iterable<Widget<ID>> widgets, IdGenerator<ID> idGenerator) throws ZIndexConflictException {
@@ -222,91 +291,98 @@ public class InMemoryWidgetRepository<ID> implements WidgetRepository<ID> {
 
     @Override
     public Widget<ID> add(WidgetCreateDto widgetDto) throws ZIndexConflictException {
-        try (var locked = rwLock.writeLock()) {
+        try (var ignored = rwLock.writeLock()) {
             return internal.add(widgetDto);
         }
     }
 
     @Override
     public Stream<Widget<ID>> add(Iterable<WidgetCreateDto> widgets) throws ZIndexConflictException {
-        try(var locked = rwLock.writeLock()) {
+        try (var ignored = rwLock.writeLock()) {
             return internal.add(widgets);
         }
     }
 
     @Override
     public Optional<Widget<ID>> save(Widget<ID> widget) throws ZIndexConflictException {
-        try(var locked = rwLock.writeLock()) {
+        try (var ignored = rwLock.writeLock()) {
             return internal.save(widget);
         }
     }
 
     @Override
     public void save(Iterable<Widget<ID>> widgets) throws ZIndexConflictException {
-        try(var locked = rwLock.writeLock()) {
+        try (var ignored = rwLock.writeLock()) {
             internal.save(widgets);
         }
     }
 
     @Override
     public Optional<Widget<ID>> getById(ID id) {
-        try (var locked = rwLock.readLock()) {
+        try (var ignored = rwLock.readLock()) {
             return internal.getById(id);
         }
     }
 
     @Override
+    public Stream<Widget<ID>> getAll() {
+        try (var ignored = rwLock.readLock()) {
+            return internal.getAll();
+        }
+    }
+
+    @Override
+    public Stream<Widget<ID>> getInArea(Area area) {
+        try (var ignored = rwLock.readLock()) {
+            return internal.getInArea(area);
+        }
+    }
+
+    @Override
     public Optional<Widget<ID>> deleteAndReturnById(ID id) {
-        try(var locked = rwLock.writeLock()) {
+        try (var ignored = rwLock.writeLock()) {
             return internal.deleteAndReturnById(id);
         }
     }
 
     @Override
     public boolean deleteById(ID id) {
-        try(var locked = rwLock.writeLock()) {
+        try (var ignored = rwLock.writeLock()) {
             return internal.deleteById(id);
         }
     }
 
     @Override
     public void deleteAll() {
-        try (var locked = rwLock.writeLock()) {
+        try (var ignored = rwLock.writeLock()) {
             internal.deleteAll();
         }
     }
 
     @Override
-    public Stream<Widget<ID>> getAll() {
-        try(var locked = rwLock.readLock()) {
-            return internal.getAll();
-        }
-    }
-
-    @Override
     public Optional<Integer> getMaxZIndex() {
-        try(var locked = rwLock.readLock()) {
+        try (var ignored = rwLock.readLock()) {
             return internal.getMaxZIndex();
         }
     }
 
     @Override
     public void shiftUpwards(Integer zIndex) throws ArithmeticException {
-        try(var locked = rwLock.writeLock()) {
+        try (var ignored = rwLock.writeLock()) {
             internal.shiftUpwards(zIndex);
         }
     }
 
     @Override
     public <T, E extends Exception> T runAtomically(AtomicFunction<WidgetRepository<ID>, T, E> action) throws E {
-        try(var locked = rwLock.writeLock()) {
+        try (var ignored = rwLock.writeLock()) {
             return action.run(internal);
         }
     }
 
     @Override
     public <E extends Exception> void runAtomically(AtomicAction<WidgetRepository<ID>, E> action) throws E {
-        try(var locked = rwLock.writeLock()) {
+        try (var ignored = rwLock.writeLock()) {
             action.run(internal);
         }
     }
